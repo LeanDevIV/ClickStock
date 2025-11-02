fc5c6c32f4c052103b90cdb8efbb6331c6c74b07
luciano mayorga <lucianoagustinmayorga@gmail.com>
2025-11-02 17:53:59 -0300

    feat(footer): crear componente Footer y agregar estilos

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React, { useEffect } from "react";
 import AppRoutes from "./routes/indexroutes.jsx";
+import Footer from "./components/Footer.jsx";
 
 function App() {
   return (
     <div>
       <AppRoutes />
+      <Footer />
     </div>
   )
 }
-
-export default App
+export default App;
