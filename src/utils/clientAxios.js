f3c22c768ff45ed0c0b35050fd70586a77ccf25f
LeanDevIV <leandrocordoba318@gmail.com>
2025-11-04 19:10:56 -0300

    refactorización visual y de código del crud de productos - Se eliminaron enlaces innecesarios en los botones de editar y eliminar - Se mejoró la estructura del código para una mayor legibilidad - Se optimizó el manejo de estados y efectos en el componente -Correccion minima de direccionamiento en clientAxios.js

--- src/utils/clientAxios.js
+++ src/utils/clientAxios.js
@@ -3,7 +3,7 @@ import { useStore } from "../hooks/useStore";
 
 const clientAxios = axios.create({
 
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
   headers: { "Content-Type": "application/json" },
 });
 
